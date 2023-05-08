91b9f29953a982c5d8c335168fb63b0462687836
Luis Iturrios <luisiturrios1@gmail.com>
2023-05-08 14:22:51 -0700

    Fix bug

--- cementerio/views/clientes/clientes.py
+++ cementerio/views/clientes/clientes.py
@@ -7,9 +7,9 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
-from elasticsearch_dsl import Q
+# from elasticsearch_dsl import Q
 
-from cementerio.documents import ClienteDocument
+# from cementerio.documents import ClienteDocument
 from cementerio.models import Cliente
 
 
--- cementerio/views/difuntos/difuntos.py
+++ cementerio/views/difuntos/difuntos.py
@@ -7,9 +7,9 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
-from elasticsearch_dsl import Q
+# from elasticsearch_dsl import Q
 
-from cementerio.documents import DifuntoDocument
+# from cementerio.documents import DifuntoDocument
 from cementerio.models import Difunto
 
 
